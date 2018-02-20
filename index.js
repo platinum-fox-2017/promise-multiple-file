f7e4c3ab89878f2eba40c149e3d1f5c7b6b0f07c
Reza Pramudhika <rezapramudhika@gmail.com>
2018-02-20 15:56:00 +0700

    Release 1 dan 2

--- index.js
+++ index.js
@@ -4,32 +4,46 @@ const fs = require('fs');
 function readFilePromise(file) {
   return new Promise(function(resolve, reject){
     fs.readFile(file, 'UTF-8', function(err, data){
-      if(err) reject(err);
-      resolve(JSON.parse(data));
+      if(err) {
+        let msg = 'Terjadi error pada proses pembacaan data';
+        reject(msg);
+      }else{
+        resolve(JSON.parse(data));
+      }
     })
   })
 }
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
   readFilePromise(parentFileName)
-    .then(function(data){
-      console.log(data);
+    .then(function(dataParent){
+      for(let i=0; i<dataParent.length; i++){
+        dataParent[i].childrens = [];
+      }
+      readFilePromise(childrenFileName)
+        .then(function(dataChildren){
+          for(let i=0; i<dataChildren.length; i++){
+            for(let j=0; j<dataParent.length; j++){
+              if(dataChildren[i].family == dataParent[j].last_name){
+                dataParent[j].childrens.push(dataChildren[i].full_name)
+              }
+            }
+          }
+          console.log(dataParent);
+        })
+        .catch(function(err){
+          console.log(err);
+        })
     })
     .catch(function(err){
       console.log(err);
     })
-  readFilePromise(childrenFileName)
-    .then(function(data){
-      console.log(data);
-    })
-    .catch(function(err){
-      console.log(err);
-    })
+  
 }
 
 matchParentsWithChildrens('./parents.json', './childrens.json');
 console.log("Notification : Data sedang diproses !");
 
 // for Release 2
-// matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
-// matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json');
+matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
+matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json');

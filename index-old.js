cad4ac9adce50d75b86b4a9e28d694ac803716c2
FTeddy <ainsophoure@gmail.com>
2018-02-20 16:14:46 +0700

    promise chaining achieved

--- index-old.js
+++ index-old.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 var sleep = require('sleep');
 
 function readFilePromise(fileName) {
+  // psst, the promise should be around here...
   return new Promise((resolve,reject) => {
     fs.readFile(fileName, 'utf8', (err,data)=>{
       if (err) {
@@ -14,18 +15,19 @@ function readFilePromise(fileName) {
 }
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
+  // your code here... (p.s. readFilePromise function(s) should be around here..)
   // read parents
   let parent_data =[];
   readFilePromise(parentFileName)
     .then((data)=>{
-      sleep.sleep(5);
+      sleep.sleep(2);
       parent_data = JSON.parse(data);
       console.log(`file ${parentFileName} read!`);
       // read childrens data
       return readFilePromise(childrenFileName)
       })
     .then((data2)=>{
-      sleep.sleep(5);
+      sleep.sleep(2);
       let children_data = JSON.parse(data2);
       console.log(`file ${childrenFileName} read!`);
 
@@ -46,6 +48,40 @@ function matchParentsWithChildrens(parentFileName, childrenFileName) {
       });
 }
 
+// function matchParentsWithChildrens(parentFileName, childrenFileName) {
+//   // your code here... (p.s. readFilePromise function(s) should be around here..)
+//   // read parents
+//   readFilePromise(parentFileName).then((data)=>{
+//     sleep.sleep(5);
+//     let parent_data = JSON.parse(data);
+//     console.log(`file ${parentFileName} read!`);
+//     // read childrens data
+//     readFilePromise(childrenFileName).then((data2) => {
+//       sleep.sleep(5);
+//       let children_data = JSON.parse(data2);
+//       console.log(`file ${childrenFileName} read!`);
+//
+//       for (let i = 0; i < parent_data.length; i++) {
+//         parent_data[i].childrens = [];
+//         for (let j = 0; j < children_data.length; j++) {
+//           if (parent_data[i].last_name === children_data[j].family) {
+//             parent_data[i].childrens.push(children_data[j].full_name);
+//           }
+//         }
+//       }
+//
+//       console.log(parent_data);
+//
+//     }).catch((err)=>{
+//       console.log(err);
+//       console.log('Terjadi error pada proses pembacaan data');
+//     });
+//
+//   }).catch((err)=>{
+//     console.log(err);
+//     console.log('Terjadi error pada proses pembacaan data');
+//   });
+// }
 
 matchParentsWithChildrens('./parents.json', './childrens.json');
 console.log("Notification : Data sedang diproses !");

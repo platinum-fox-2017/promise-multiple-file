e302b082c97cf9c3907e0b3fbee8812909966972
haripermadi <harinurpermadi@gmail.com>
2018-02-20 15:24:56 +0700

    release 0

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const fs = require('fs');
 var sleep = require('sleep');
 
-function readFilePromise() {
+function readFilePromise(file) {
   // psst, the promise should be around here...
+  return new Promise((resolve,reject)=>{
+    fs.readFile(file, 'utf-8',function(err,data){
+      if(err){
+        reject(err)
+      }
+      else{
+        let dataFile = JSON.parse(data)
+        resolve(dataFile)
+      }
+    })
+  })
 }
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
   // your code here... (p.s. readFilePromise function(s) should be around here..)
+  readFilePromise(parentFileName).then(dataParent=>{
+    
+  })
 }
 
 matchParentsWithChildrens('./parents.json', './childrens.json');
+
 console.log("Notification : Data sedang diproses !");
 
+// readFilePromise('parents.json').then(dataFile=>{
+//   console.log(dataFile)
+// }).catch(err=>{
+//   console.log(err)
+// })
 // for Release 2
-matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
-matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json');
+// matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
+// matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json');

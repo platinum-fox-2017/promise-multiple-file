bcf4b32666c6075fa2f8aaf1c4c98c46505dbb0b
Herby Herado <herby,herado@gmail.com>
2018-02-20 17:41:36 +0700

    Release 2

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-const fs = require('fs');
-var sleep = require('sleep');
+// import { resolve } from 'path';
 
-function readFilePromise() {
+const fs = require('fs');
+// var sleep = require('sleep');
+
+function readFilePromise(file) {
   // psst, the promise should be around here...
+  return readFile = new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) reject('Terjadi error pada proses pembacaan data')
+      else {
+        resolve(JSON.parse(data))
+      }
+    })
+  })
 }
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
   // your code here... (p.s. readFilePromise function(s) should be around here..)
+
+  readFilePromise(parentFileName)
+  .then(parent_data => {
+    // sleep.sleep(5)
+    readFilePromise(childrenFileName).then(children_data => {
+      for (let j = 0; j < parent_data.length; j++){
+        parent_data[j].childrens = []
+      }
+      for (let i = 0; i < children_data.length; i++){
+        switch (children_data[i].family){
+          case 'Campbell': parent_data[0].childrens.push(children_data[i].full_name); break;
+          case 'Katsev': parent_data[1].childrens.push(children_data[i].full_name); break;
+          case 'Robin': parent_data[2].childrens.push(children_data[i].full_name); break;
+          case 'Oliver': parent_data[3].childrens.push(children_data[i].full_name); break;
+          case 'Wood': parent_data[4].childrens.push(children_data[i].full_name); break;
+          case 'Bass': parent_data[5].childrens.push(children_data[i].full_name); break;
+          case 'Caldwell': parent_data[6].childrens.push(children_data[i].full_name); break;
+        }
+      }
+      console.log(parent_data)
+    }).catch(err => console.log(err))
+  }).catch(err => console.log(err))
 }
 
 matchParentsWithChildrens('./parents.json', './childrens.json');

1b71fecdd741358a7e30f6c378f635d19223ac48
kevinhimawanhie <kevinhimawanhie@gmail.com>
2018-02-20 17:28:54 +0700

    Feedback

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 const fs = require('fs');
 var sleep = require('sleep');
 
-function readFilePromise() {
-  // psst, the promise should be around here...
+function readFilePromise(data) {
+  return new Promise ((resolve,reject)=>{
+    fs.readFile(data,'UTF-8', (err,readData)=>{
+      if(readData){
+        let parse = JSON.parse(readData)
+        resolve(parse)
+      }else{
+        reject(`terjadi error pada saat pembacaan data`)
+      }
+    })
+  })
 }
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
-  // your code here... (p.s. readFilePromise function(s) should be around here..)
+  return new Promise ((resolve,reject)=>{
+    readFilePromise(parentFileName).then((parentData)=>{
+      readFilePromise(childrenFileName).then((childrenData)=>{
+        let hasil = []
+        parentData.forEach(each=>{
+          let tmp = []
+          childrenData.forEach(eachChildren=>{
+            if(each.last_name === eachChildren.family){
+              tmp.push(eachChildren.full_name)
+            }
+          })
+          each['children'] = tmp
+          hasil.push(each)
+        })
+          resolve(hasil)
+      })
+    }).catch((err)=>{reject(err)})
+  })  
 }
 
-matchParentsWithChildrens('./parents.json', './childrens.json');
-console.log("Notification : Data sedang diproses !");
+// matchParentsWithChildrens('./parents.json', './childrens.json').then((data)=>{
+//   console.log(data)
+// })
+// console.log("Notification : Data sedang diproses !");
+// sleep.sleep(1)
 
 // for Release 2
-matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
-matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json');
+matchParentsWithChildrens('./parents.json', './not_a_real_file.json').then(()=>{
+
+}).catch((err)=>{console.log(err)})
+matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json').then(()=>{
+
+}).catch((err)=>{console.log(err)})

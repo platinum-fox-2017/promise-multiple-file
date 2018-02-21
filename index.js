2c7d804ffcd60612e64681180517077ab33ecc5e
davidyunius <32894219+davidyunius@users.noreply.github.com>
2018-02-21 11:07:13 +0700

    release 0

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 const fs = require('fs');
 var sleep = require('sleep');
 
-function readFilePromise() {
+function readFilePromise(parentFileName, childrenFileName) {
   // psst, the promise should be around here...
+  return new Promise((resolve, reject) => {
+    fs.readFile(parentFileName, 'UTF-8', (errParent, dataParent) => {
+      if (errParent) {
+        reject(errParent)
+      } else {
+        fs.readFile(childrenFileName, 'UTF-8', (errChild, dataChild) => {
+          if (errChild) {
+            reject(errChild)
+          } else {
+            convertDataParent = JSON.parse(dataParent)
+            convertDataChild = JSON.parse(dataChild)
+            let dataExport = { convertDataParent, convertDataChild }
+            resolve(dataExport)
+          }
+        })
+      }
+
+    })
+  })
 }
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
   // your code here... (p.s. readFilePromise function(s) should be around here..)
+  readFilePromise(parentFileName, childrenFileName).then(result => {
+    let dataFamily = []
+    for (let i = 0; i < result.convertDataParent.length; i++) {
+      result.convertDataParent[i].childrens = []
+    }
+
+    for (let i = 0; i < result.convertDataParent.length; i++) {
+      for (let j = 0; j < result.convertDataChild.length; j++) {
+        if (result.convertDataChild[j].family === result.convertDataParent[i].last_name) {
+          result.convertDataParent[i].childrens.push(result.convertDataChild[j].full_name)
+        }
+      }
+    }
+    sleep.sleep(5)
+    console.log(result.convertDataParent)
+  }).catch(err => {
+    console.log(err);
+
+  })
 }
 
 matchParentsWithChildrens('./parents.json', './childrens.json');
 console.log("Notification : Data sedang diproses !");
 
 // for Release 2
-matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
-matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json');
+// matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
+// matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json');
